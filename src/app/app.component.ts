14b6118f110c613fc369af37e260850b1cb27cf8
gru <grujica.vasic@hotmail.com>
2025-10-10 14:57:16 +0200

    App setup and Material install

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MapComponent } from "./components/map/map.component";
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './pages/main/main.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    MapComponent,
+    HeaderComponent,
+    MainComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
