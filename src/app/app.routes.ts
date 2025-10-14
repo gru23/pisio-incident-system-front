d8e6a5610e56a2ba3ed130096ca2ef799789424d
gru <grujica.vasic@hotmail.com>
2025-10-14 15:49:11 +0200

    adding routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'main',
+        pathMatch: 'full'
+    },
+    {
+        path: 'main',
+        loadComponent: () => import('./pages/main/main.component').then(m => m.MainComponent),
+        title: 'Incidents',
+    },
+    {
+        path: 'moderator',
+        loadComponent: () => import('./pages/moderator/moderator.component').then(m => m.ModeratorComponent),
+        title: 'Moderation',
+    },
+];
