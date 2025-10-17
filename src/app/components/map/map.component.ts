ef75f1a4c0663f59fba5d80c5b423b68192302f7
gru <grujica.vasic@hotmail.com>
2025-10-17 13:28:58 +0200

    output location from map component to main component

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, PLATFORM_ID, Inject } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  PLATFORM_ID,
+  Inject,
+  Output,
+  EventEmitter
+} from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
 @Component({
@@ -9,6 +16,8 @@ import { isPlatformBrowser } from '@angular/common';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+  @Output() locationSelected = new EventEmitter<{ lat: number; lng: number }>();
+
   private isBrowser: boolean;
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {
@@ -18,32 +27,45 @@ export class MapComponent implements OnInit {
   async ngOnInit(): Promise<void> {
     if (!this.isBrowser) return;
 
-  const L = await import('leaflet');
+    const L = await import('leaflet');
 
-  const defaultIcon = L.icon({
-    iconUrl: 'assets/leaflet/marker-icon.png',
-    iconRetinaUrl: 'assets/leaflet/marker-icon-2x.png',
-    shadowUrl: 'assets/leaflet/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    tooltipAnchor: [16, -28],
-    shadowSize: [41, 41]
-  });
+    // Default marker ikona
+    const defaultIcon = L.icon({
+      iconUrl: 'assets/leaflet/marker-icon.png',
+      iconRetinaUrl: 'assets/leaflet/marker-icon-2x.png',
+      shadowUrl: 'assets/leaflet/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      tooltipAnchor: [16, -28],
+      shadowSize: [41, 41]
+    });
 
-  L.Marker.prototype.options.icon = defaultIcon;
+    L.Marker.prototype.options.icon = defaultIcon;
 
-  const map = L.map('map').setView([44.7866, 20.4489], 13);
+    const map = L.map('map').setView([44.766582, 17.186928], 13);
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors'
-  }).addTo(map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
 
-  L.marker([44.7866, 20.4489])
-    .addTo(map)
-    .bindPopup('Beograd')
-    .openPopup();
-    
+    // Dodaj prazan marker koji će se ažurirati
+    let marker: L.Marker | null = null;
+
+    // Klik na mapu
+    map.on('click', (e: L.LeafletMouseEvent) => {
+      const { lat, lng } = e.latlng;
+
+      if (marker) {
+        marker.setLatLng([lat, lng]);
+      } else {
+        marker = L.marker([lat, lng]).addTo(map);
+      }
+
+      this.locationSelected.emit({ lat, lng });
+    });
+
+    // Popravi dimenzije ako treba
     setTimeout(() => {
       map.invalidateSize();
     }, 0);
